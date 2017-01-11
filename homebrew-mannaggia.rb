df2076b22a499764df936435d350d90a73baf2a6
Matteo Merola <mattmezza@gmail.com>
2017-01-12 00:23:50 +0100

    v1.0.3 Formula

---
+++ homebrew-mannaggia.rb
@@ -0,0 +1,15 @@
+class HomebrewMannaggia < Formula
+  desc "Automatic saint calling for depressed developers, in italian."
+  homepage "https://github.com/mattmezza/homebrew-mannaggia"
+  url "https://github.com/mattmezza/homebrew-mannaggia/archive/v1.0.3.tar.gz"
+  sha256 "0164155f8d09e9f9edc76543e587dc60080d0b1de116195039d9ff2ab953a4d3"
+
+  def install
+    bin.install "mannaggia.sh", "santiebeati"
+  end
+
+  test do
+
+  end
+
+end
